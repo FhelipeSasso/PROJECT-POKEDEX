42784651a65878424532aaab9b8b592f5c4246a1
FhelipeSasso <saselegendas@outlook.com>
2023-11-12 13:34:03 -0300

    organizando

--- Pokedex.py
+++ Pokedex.py
@@ -60,21 +60,11 @@ def updating_window_color(pokemon_type_bg):
 # FUNCTION TO LOAD POKEMON
 
 
-def load_pokemon():
+def loadPokemon():
     # Pokemon info based on the user input
     pokemon = pypokedex.get(name=text_id_name.get(1.0, "end-1c"))
     # color associated to the pokemon type
     pokemon_type_bg = pokemon.types[0].title()
-
-    # get pokemon img
-    http = urllib3.PoolManager()
-    response = http.request('GET', pokemon.sprites.front.get('default'))
-    image = PIL.Image.open(BytesIO(response.data))
-
-    # show pokemon img
-    img = PIL.ImageTk.PhotoImage(image)
-    pokemon_image.config(image=img, bg=window.cget('bg'))  # box background
-    pokemon_image.image = img
 
     # Get the color associated with the Pokemon type
     box_color = updating_window_color(pokemon_type_bg)
@@ -87,51 +77,43 @@ def load_pokemon():
     # pokemon info like: pokedex number, type, ability and name
     pokemon_information.config(text=f"#{pokemon.dex} - {pokemon.name}".title())
     pokemon_types.config(text=" - ".join([t for t in pokemon.types]).title())
-    pokemon_ability.config(text=" \n ".join([ability.name for ability in pokemon.abilities]).title())
-
-    pokemon = pypokedex.get(name=text_id_name.get(1.0, "end-1c"))
-    pokemon_type_bg = pokemon.types[0].title()
+    pokemon_ability.config(text="\n".join([ability.name for ability in pokemon.abilities]).title())
 
     http = urllib3.PoolManager()
     response = http.request('GET', pokemon.sprites.front.get('default'))
     image = PIL.Image.open(BytesIO(response.data))
 
     img = PIL.ImageTk.PhotoImage(image)
-    pokemon_image.config(image=img, bg=window.cget('bg'))
+    pokemon_image.config(image=img, bg=box_color)
     pokemon_image.image = img
 
     # get the color associated with the Pokemon type
     box_color = updating_window_color(pokemon_type_bg)
+    # title background associated with the pokemon type 
+    title_label.config(bg=box_color)
+    # insert associated with pokemon type
+    label_id_name.config(font=("Arial", 20), bg=box_color)
 
-    # set the background color of the box/label
-    pokemon_information.config(bg=box_color)
-    pokemon_types.config(bg=box_color)
-    pokemon_ability.config(bg=box_color)
-    # set title accordingly to the pokemon type
-    title_label.config(bg=window.cget('bg'))
-
-    pokemon_information.config(text=f"#{pokemon.dex} - {pokemon.name}".title())
-    pokemon_types.config(text=" - ".join([t for t in pokemon.types]).title())
-    pokemon_ability.config(text="\n".join([ability.name for ability in pokemon.abilities]).title())
-
-def clearText(event=None):
+def clearText():
     text_id_name.delete("1.0", tk.END)
 
-label_id_name = tk.Label(window, text="Insert Pokemon")
+    
+label_id_name = tk.Label(window, text="Please insert the Pokémon name or number in the box below")
 label_id_name.config(font=("Arial", 20))
 label_id_name.pack(padx=10, pady=10)
 
 text_id_name = tk.Text(window, height=0.5)
 text_id_name.config(font=("Arial", 20))
 text_id_name.pack(padx=10, pady=10)
-text_id_name.bind("<Return>", lambda event=None: load_pokemon())
+text_id_name.bind("<Return>", lambda event=None: loadPokemon())
 
-button_load = tk.Button(window, text="Find Pokemon", command=load_pokemon)
+button_load = tk.Button(window, text="Find Pokemon", command=loadPokemon)
 button_load.config(font=("Arial", 20))
 button_load.pack(padx=10, pady=10)
 
-clear_button = tk.Button(window, text="Clear Entry Box", command=clearText)
+clear_button = tk.Button(window, text="Clear entry box", command=clearText)
 clear_button.config(font=("Arial", 10))
 clear_button.pack()
+
 
 window.mainloop()
